41248dfc922b3b3d2734d338a298b76d3df66b0e
Sean McMains <sean@mcmains.net>
2021-12-01 09:04:20 -0600

    Day 1, part 2
    refactored existing

--- AoC2021Container/AoC2021Container/Day1.playground/Contents.swift
+++ AoC2021Container/AoC2021Container/Day1.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Algorithms
 
 // read and parse data
 let fileURL = Bundle.main.url(forResource: "Input", withExtension: "txt")
@@ -6,18 +7,18 @@ let content = try String(contentsOf: fileURL!, encoding: String.Encoding.utf8)
 let contentArray = content.split(separator: "\n").compactMap { Int($0) }
 
 // part 1
-print(contentArray.ascendingTally())
+let pairs = contentArray.windows(ofCount: 2)
+print(tallyIncreasingPairs(pairs))
 
-extension Array where Element == Int {
-    func ascendingTally() -> Int {
-        var tally1 = 0
+// part 2
+let dataWindows = contentArray.windows(ofCount: 3)
+let dataWindowSums = dataWindows.map { $0.reduce(0, +) }
+let sumPairs = dataWindowSums.windows(ofCount: 2)
+print(tallyIncreasingPairs(sumPairs))
 
-        (1..<contentArray.count).forEach { i in
-            if contentArray[i] > contentArray[i-1] {
-                tally1 += 1
-            }
-        }
-
-        return tally1
+func tallyIncreasingPairs( _ pairs: Windows<[Int]>) -> Int {
+    pairs.reduce(0) { partialResult, pair in
+        let increase = pair.last! > pair.first! ? 1 : 0
+        return partialResult + increase
     }
 }
