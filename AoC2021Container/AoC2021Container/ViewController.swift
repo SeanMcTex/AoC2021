5b1a523a06654580cb8e89fd1705c1650805e010
Sean McMains <sean@mcmains.net>
2021-12-01 08:32:33 -0600

    Add container project

---
+++ AoC2021Container/AoC2021Container/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AoC2021Container
+//
+//  Created by Sean Mc Mains on 12/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
